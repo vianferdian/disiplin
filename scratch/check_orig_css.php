39a574d515319e73c0b9cfd426039fa389b68623
vianferdian <vianferdian@gmail.com>
2026-09-07 22:11:16 +0700

    clean up temporary scratch files

--- scratch/check_orig_css.php
+++
@@ -1,6 +0,0 @@
-<?php
-$origPath = __DIR__ . '/../../assets/css/style.css';
-echo "Orig exists: " . (file_exists($origPath) ? 'YES' : 'NO') . "\n";
-if (file_exists($origPath)) {
-    echo "Orig size: " . filesize($origPath) . " bytes\n";
-}
